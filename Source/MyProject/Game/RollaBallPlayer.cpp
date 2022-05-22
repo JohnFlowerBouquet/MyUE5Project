6f7f9d9cc4c38d38fe12b67d48bf1593f5746b63
John <john.flower.bouquet@gmail.com>
2022-05-22 21:32:22 +0200

    Course: Updated RollaBallPlayer class with core components & actions

--- Source/MyProject/Game/RollaBallPlayer.cpp
+++ Source/MyProject/Game/RollaBallPlayer.cpp
@@ -1,34 +1,53 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "RollaBallPlayer.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ARollaBallPlayer::ARollaBallPlayer()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
 
+	// Create components
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	// Components configuration
+	RootComponent = Mesh;
+	SpringArm->SetupAttachment(Mesh);
+	Camera->SetupAttachment(SpringArm);
 }
 
 // Called when the game starts or when spawned
 void ARollaBallPlayer::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
-void ARollaBallPlayer::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+// void ARollaBallPlayer::Tick(float DeltaTime)
+// {
+// 	Super::Tick(DeltaTime);
 
-}
+// }
 
 // Called to bind functionality to input
-void ARollaBallPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ARollaBallPlayer::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
+void ARollaBallPlayer::MoveRight(float Value)
+{
+}
+
+void ARollaBallPlayer::MoveForward(float Value)
+{
+}
+
+void ARollaBallPlayer::Jump()
+{
+}
